c08d5608dd1a8a1d650122dd77be55748950bc88
Louis Barnes <101646269+LouisBarnes@users.noreply.github.com>
2022-05-12 11:49:43 +0100

    Reentrance.sol

---
+++ Reentrance/Reentrance.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+// Updated solidity to latest version from 0.6.0
+pragma solidity ^0.8.0;
+
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/3bdf4bfd29b145288ec741bef259a384007ed5cf/contracts/utils/math/SafeMath.sol';
+
+contract Reentrance {
+  
+  using SafeMath for uint256;
+  mapping(address => uint) public balances;
+
+  function donate(address _to) public payable {
+    balances[_to] = balances[_to].add(msg.value);
+  }
+
+  function balanceOf(address _who) public view returns (uint balance) {
+    return balances[_who];
+  }
+
+  function withdraw(uint _amount) public {
+    if(balances[msg.sender] >= _amount) {
+      (bool result,) = msg.sender.call{value:_amount}("");
+      if(result) {
+        _amount;
+      }
+      balances[msg.sender] -= _amount;
+    }
+  }
+
+  receive() external payable {}
+}
