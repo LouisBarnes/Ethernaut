66471171bdceeb28ff3307dd5da97b55dc22218a
Louis Barnes <101646269+LouisBarnes@users.noreply.github.com>
2022-06-17 10:21:05 +0100

    Update GuessCoinFlip.sol

--- CoinFlip/GuessCoinFlip.sol
+++ CoinFlip/GuessCoinFlip.sol
@@ -3,14 +3,10 @@
 // I have used the latest version of solidity with this contract
 pragma solidity ^0.8.0;
 
-// Although it was not required, I have imported SafeMath to ensure good practice is used.
-import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/3bdf4bfd29b145288ec741bef259a384007ed5cf/contracts/utils/math/SafeMath.sol';
 // Import the original contract that is running 0.8.0 or above and using the correct import URL for SafeMath
 import './CoinFlip.sol';
 
 contract GuessCoinFlip {
-  // Using SafeMath on uint256 just like the contract we are targeting.
-  using SafeMath for uint256;
 
   CoinFlip public TargetContract;
   // Same factor number that is located in the target contract (this must be exactly the same) 
